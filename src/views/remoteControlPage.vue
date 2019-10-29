17b478d3d853309476ed32174d3ede78109d4f2b
zhangh <543985125@qq.com>
2019-10-29 17:23:49 +0800

    add  button

--- src/views/remoteControlPage.vue
+++ src/views/remoteControlPage.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-      this is remote page
+    <el-switch
+      v-model="openLight"
+      active-color="#13ce66"
+      :active-text="statusText"
+      inactive-color="#b3c0d1"
+      @change='onLightStatusChanged'
+    ></el-switch>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      openLight: false,
+      statusText: "打开照明"
+    };
+  },
+  methods: {
+    onLightStatusChanged(){
+      if(this.openLight){
+        this.statusText = '已打开照明'
+      }else{
+        this.statusText = '已关闭照明'
+      }
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
