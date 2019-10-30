33d4b4f089a201badef535233cdaa4392e1b27a0
zhangh <543985125@qq.com>
2019-10-30 17:57:22 +0800

    修改样式

--- src/views/remoteControlPage.vue
+++ src/views/remoteControlPage.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    <el-switch
-      v-model="openLight"
-      active-color="#13ce66"
-      :active-text="statusText"
-      inactive-color="#b3c0d1"
-      @change='onLightStatusChanged'
-    ></el-switch>
+    <el-row :gutter="10">
+      <el-col>
+        <el-switch
+          v-model="lightStatus"
+          active-color="#13ce66"
+          :active-text="lightStatusText"
+          inactive-color="#b3c0d1"
+          @change="onLightStatusChanged"
+        ></el-switch>
+      </el-col>
+      <el-col>
+        <el-switch
+          v-model="changeOfAirStatus"
+          active-color="#13ce66"
+          :active-text="changeOfAirStatusText"
+          inactive-color="#b3c0d1"
+          @change="onChangeOfAirStatusChanged"
+        ></el-switch>
+      </el-col>
+      <el-col>
+        <el-switch
+          v-model="keepTempStatus"
+          active-color="#13ce66"
+          :active-text="keepTempStatusText"
+          inactive-color="#b3c0d1"
+          @change="onKeepTempStatusChanged"
+        ></el-switch>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -14,16 +36,34 @@
 export default {
   data() {
     return {
-      openLight: false,
-      statusText: "打开照明"
+      lightStatus: false,
+      lightStatusText: "已关闭照明",
+      changeOfAirStatus: false,
+      changeOfAirStatusText: "已关闭换气",
+      keepTempStatus: false,
+      keepTempStatusText: "已关闭恒温",
     };
   },
   methods: {
-    onLightStatusChanged(){
-      if(this.openLight){
-        this.statusText = '已打开照明'
-      }else{
-        this.statusText = '已关闭照明'
+    onLightStatusChanged() {
+      if (this.lightStatus) {
+        this.lightStatusText = "已打开照明";
+      } else {
+        this.lightStatusText = "已关闭照明";
+      }
+    },
+    onChangeOfAirStatusChanged() {
+      if (this.changeOfAirStatus) {
+        this.changeOfAirStatusText = "已打开换气";
+      } else {
+        this.changeOfAirStatusText = "已关闭换气";
+      }
+    },
+    onKeepTempStatusChanged() {
+      if (this.keepTempStatus) {
+        this.keepTempStatusText = "已打开恒温";
+      } else {
+        this.keepTempStatusText = "已关闭恒温";
       }
     }
   }
@@ -31,4 +71,8 @@ export default {
 </script>
 
 <style>
+.el-col {
+  margin-left: 10px;
+  margin-top: 10px;
+}
 </style>
--- src/views/statusPage.vue
+++ src/views/statusPage.vue
@@ -1,15 +1,70 @@
 <template>
-  <div>
-      this is status page
+  <div class="statusInfo">
+    <el-row :gutter="20">
+      <div class="content">
+        <el-col :span="6">
+          <el-card class="dht11">
+            <!-- 温度显示区域 -->
+            <div slot="header">asd</div>
+            <div></div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="dht11">
+            <!-- 可燃气体显示区域 -->
+            <div slot="header">a222</div>
+            <div></div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="dht11">
+            <!-- 宠物在窝显示区域 -->
+            <div slot="header">a222</div>
+            <div></div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="dht11">
+            <!-- 可燃气体显示区域 -->
+            <div slot="header">a222</div>
+            <div></div>
+          </el-card>
+        </el-col>
+        <el-col :span="6">
+          <el-card class="dht11">
+            <!-- 宠物在窝显示区域 -->
+            <div slot="header">a222</div>
+            <div></div>
+          </el-card>
+        </el-col>
+      </div>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+/* .el-row {
+  margin-top: 10px;
+} */
+.statusInfo {
+  height: 100vh;
+  text-align: center;
+}
+.el-col{
+  justify-content: flex-start;
+}
+/* .el-card {
+  margin-top: 10px;
+} */
+.content{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 
+  flex-wrap: wrap;
+}
 </style>
